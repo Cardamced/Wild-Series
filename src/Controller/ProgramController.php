734797b285f1a38cc7ac6bf6ad6100328e839c20
Cardamced <120644423+Cardamced@users.noreply.github.com>
2023-05-22 12:55:52 +0200

    created routes and show view

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -11,15 +11,24 @@ use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\Routing\Annotation\Route;
 
-
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'program_index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
+        ]);
+    }
+
+    #[Route('/{id}', requirements: ['id' => '\d+'], methods: ['GET'], name: 'program_show')]
+    
+    public function show($id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
         ]);
     }
 }

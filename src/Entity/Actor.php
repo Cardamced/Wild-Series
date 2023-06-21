46e70450cbf716fd218eb131e0fd341bb5684d33
Cardamced <120644423+Cardamced@users.noreply.github.com>
2023-06-21 13:39:57 +0200

    navbar component twig

--- src/Entity/Actor.php
+++ src/Entity/Actor.php
@@ -67,4 +67,16 @@ class Actor
 
         return $this;
     }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 }
